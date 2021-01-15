cc341ff2d938c8599270a1477c41a001f514cb77
Ningensei848 <k.kubokawa@klis.tsukuba.ac.jp>
2021-01-15 19:24:56 +0900

    delete/these files were too complex for me :(

--- .config/jest.config.ts
+++
@@ -1,9 +0,0 @@
-import type { Config } from "@jest/types";
-
-const neutrino = require("neutrino");
-process.env.NODE_ENV = process.env.NODE_ENV || "test";
-
-// Or async function
-export default async (): Promise<Config.InitialOptions> => {
-  return neutrino().jest();
-};
