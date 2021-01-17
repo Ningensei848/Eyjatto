e3e1244cf66e559d880cee58906169b0d3c661a2
Ningensei848 <k.kubokawa@klis.tsukuba.ac.jp>
2021-01-17 15:09:59 +0900

    update / Node.js 14.15 and Yarn  1.22.10

--- jest.config.ts
+++ jest.config.ts
@@ -6,21 +6,29 @@ import { pathsToModuleNameMapper } from "ts-jest/utils"
 // cf. https://www.typescriptlang.org/ja/tsconfig#moduleResolution
 import { compilerOptions } from "./tsconfig.json"
 
-// const paths: Array<string> =
-
-// Configuration: cf. https://jestjs.io/docs/ja/configuration
+/** @typedef {import('ts-jest/dist/types')} */
+/** @type {import('@jest/types').Config.InitialOptions} */
 const config = (): Config.InitialOptions => {
+  // jest configuration goes here: cf. https://jestjs.io/docs/ja/configuration
   return {
     verbose: true,
+    notify: true,
     preset: "ts-jest",
+    rootDir: ".",
     moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths),
     testMatch: [...defaults.testMatch, "**/test/**/*.[jt]s?(x)"],
+    testPathIgnorePatterns: [...defaults.testPathIgnorePatterns, "/dist/"],
+    testEnvironment: "node",
+    // globalSetup: "<rootDir>/test/setup.ts",
+    // globalTeardown: "<rootDir>/test/teardown.ts",
     globals: {
       "ts-jest": {
-        // ts-jest configuration goes here
-        useESM: true
+        // ts-jest configuration goes here: cf. https://kulshekhar.github.io/ts-jest/docs/options
+        tsconfig: "./.config/tsconfig.jest.json",
+        useEsm: true
       }
     }
+    // reporters: ["default", "jest-failure-reporter"],
   }
 }
 
